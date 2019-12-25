ad823b7cf58326c1602c483c716ad3ca53414c28
Anna Tezelashvili <tezelashviliann@gmail.com>
2019-12-25 17:28:25 +0400

    Initial commit

---
+++ app/src/main/java/ge/edu/tsu/starbucksapp/LoginActivity.java
@@ -0,0 +1,16 @@
+package ge.edu.tsu.starbucksapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_login);
+
+    }
+}

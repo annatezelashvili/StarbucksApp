8856073c5b72a58aa356ace33263d6a041de88c8
Anna Tezelashvili <tezelashviliann@gmail.com>
2019-12-28 03:56:48 +0400

    modified contactus activity

--- app/src/main/java/ge/edu/tsu/starbucksapp/ActivityContactUs.java
+++ app/src/main/java/ge/edu/tsu/starbucksapp/ActivityContactUs.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +16,20 @@ public class ActivityContactUs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
     }
-    public void openBrowser(View view){
-        String url = (String)view.getTag();
+
+    public void openBrowser(View view) {
+        String url = (String) view.getTag();
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
         intent.addCategory(Intent.CATEGORY_BROWSABLE);
         intent.setData(Uri.parse(url));
         startActivity(intent);
     }
+//
+//    public void phoneCall(View view){
+//        TextView phoneNumber=(findViewById(R.id.phone_number);
+//        String mPhoneNumber=phoneNumber.toString();
+//        Uri number = Uri.parse(StringBuilder("tel:%s",mPhoneNumber));
+//        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+//    }
 }
